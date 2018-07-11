b420aa021c819f46e51aeb9bb72b8a771466ccbc
huang <huangzengmeng@hongte.info>
2018-07-11 21:22:12 +0800

    执行交付脚本

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -13,5 +13,6 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-java -jar target/hello-jenkins-0.0.1-SNAPSHOT.jar
+echo | pwd
+java -jar ./target/hello-jenkins-0.0.1-SNAPSHOT.jar
 
