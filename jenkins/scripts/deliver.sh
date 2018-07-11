1fca6c7119d0edabf0df69f1cd1d21f81522624d
huang <huangzengmeng@hongte.info>
2018-07-11 21:10:23 +0800

    执行交付脚本

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -5,7 +5,7 @@ echo 'into the local Maven repository, which will ultimately be stored in'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
 echo 'volume).'
 set -x
-mvn jar:jar install:install help:evaluate -Dexpression=hello-jenkins
+mvn jar:jar install:install help:evaluate -Dexpression=hello-jenkins-1.0.0.jar
 set +x
 
 echo 'The following complex command extracts the value of the <name/> element'
